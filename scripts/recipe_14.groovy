fd5a30cfac61ae58d4b9e98332ed58e7fa2974e8
liazhang <304957127@qq.com>
2014-12-02 15:16:58 +0800

    groovy session 2: fix small bug

--- scripts/recipe_14.groovy
+++ scripts/recipe_14.groovy
@@ -2,7 +2,7 @@ def show = { first, second ->
     println "first value is $first; second value is $second"
 }
 
-def showWithFirstValueIs0 = show.curry 1
+def showWithFirstValueIs0 = show.curry 0
 
 println "class name of sumStartWith0: ${showWithFirstValueIs0.class.name}"
 println "superclass name of sumStartWith0: ${showWithFirstValueIs0.class.superclass.name}"
